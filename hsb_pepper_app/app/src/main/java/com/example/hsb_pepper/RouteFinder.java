a0597c4bb85ad637fa7334ea6e5ca46c11d8e96c
Jacob B. Menge <36003126+JacobMenge@users.noreply.github.com>
2021-10-28 10:28:12 +0200

    add routefinder java class

---
+++ hsb_pepper_app/app/src/main/java/com/example/hsb_pepper/RouteFinder.java
@@ -0,0 +1,25 @@
+package com.example.hsb_pepper;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import com.aldebaran.qi.sdk.QiContext;
+import com.aldebaran.qi.sdk.QiSDK;
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
+import com.aldebaran.qi.sdk.builder.ListenBuilder;
+import com.aldebaran.qi.sdk.builder.PhraseSetBuilder;
+import com.aldebaran.qi.sdk.design.activity.RobotActivity;
+import com.aldebaran.qi.sdk.object.conversation.Bookmark;
+import com.aldebaran.qi.sdk.object.conversation.Chat;
+import com.aldebaran.qi.sdk.object.conversation.Listen;
+import com.aldebaran.qi.sdk.object.conversation.ListenResult;
+import com.aldebaran.qi.sdk.object.conversation.PhraseSet;
+import com.aldebaran.qi.sdk.object.conversation.QiChatbot;
+import com.aldebaran.qi.sdk.util.PhraseSetUtil;
+
+public class RouteFinder {
+
+    
+
+}
